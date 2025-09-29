ccf5b068b4409ddc78c358d20237e9956c434ca0
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2025-09-29 11:57:04 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE  Crud_Mundo;
-USE Crud_mundo;
+CREATE DATABASE  bd_mundo;
+USE bd_mundo;
 
 CREATE TABLE tb_pais(
-Id_pais INT AUTO_INCREMENT PRIMARY  KEY,
-nome_oficial VARCHAR(120) NOT NULL,
+id_pais INT AUTO_INCREMENT PRIMARY  KEY,
+nome VARCHAR(120) NOT NULL,
 continente VARCHAR(160) NOT NULL,
 populacao FLOAT NOT NULL,
-idioma_principal VARCHAR(60) NOT NULL
+idioma VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE tb_cidade(
-Id_cidade INT AUTO_INCREMENT PRIMARY  KEY,
+id_cidade INT AUTO_INCREMENT PRIMARY  KEY,
 nome VARCHAR(120) NOT NULL,
 populacao FLOAT NOT NULL,
-pais INT NOT NULL,
-FOREIGN KEY (pais) REFERENCES tb_pais (Id_pais)
+id_pais INT NOT NULL,
+FOREIGN KEY (id_pais) REFERENCES tb_pais (id_pais)
 );
 
 ALTER TABLE tb_pais
@@ -23,7 +23,7 @@ MODIFY populacao INT NOT NULL;
 ALTER TABLE tb_cidade
 MODIFY populacao INT NOT NULL;
 
-INSERT INTO tb_pais (nome_oficial, continente, populacao, idioma_principal)
+INSERT INTO tb_pais (nome, continente, populacao, idioma)
 VALUES 
 ('Brasil', 'América do Sul', 203100000, 'Português'),
 ('Estados Unidos', 'América do Norte', 331900000, 'Inglês'),
@@ -31,7 +31,7 @@ VALUES
 ('Alemanha', 'Europa', 83200000, 'Alemão'),
 ('Nigéria', 'África', 223800000, 'Inglês');
 
-INSERT INTO tb_cidade (nome, populacao, pais)
+INSERT INTO tb_cidade (nome, populacao, id_pais)
 VALUES
 ('São Paulo', 12300000, 1),
 ('Rio de Janeiro', 6700000, 1),
@@ -45,10 +45,10 @@ VALUES
 ('Abuja', 3600000, 5);
 
 UPDATE tb_cidade SET nome="Lagos" Where nome="Laguinhos"  ;
-UPDATE tb_pais SET idioma_principal="Inglês" Where idioma_principal="Britânico"  ;
+UPDATE tb_pais SET idioma="Inglês" Where idioma="Britânico"  ;
 
-DELETE FROM tb_cidade WHERE pais=5;
-DELETE FROM tb_pais WHERE Id_pais=5;
+DELETE FROM tb_cidade WHERE id_pais=5;
+DELETE FROM tb_pais WHERE id_pais=5;
 
 SELECT * FROM tb_cidade;
 SELECT * FROM tb_pais;
