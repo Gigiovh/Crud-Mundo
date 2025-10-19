f689a141ff50eb242a97bd4f468de591b42b4de2
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2025-10-18 23:01:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Caprasimo&display=swap" rel="stylesheet">
 </head>
-<body>
+<body style="background-color:#fff9f9">
    
-<!-- navegação para cidade e paises -->
+<!-- navegação  -->
 <br><br>   
 <nav class="test">
   <ul>
@@ -31,67 +33,87 @@
       </ul>
   </ul>
 </nav>
-<br><br>
- <h1>Bem-vindo ao BD Mundo</h1>
+<!-- Baner -->
+ <div class="banner-container">
+  <div class="banner-content">
+    <h1 class="banner-title">DESIGN<br>BANNER</h1>
+    <p class="banner-tag">CSS ONLY</p>
+  </div>
+</div>
 <!-- Card a respeito de alguns países -->
  <section class="cards-container">
 <article class="card">
   <img
     class="card__background"
-    src="https://i.imgur.com/QYWAcXk.jpeg"
-    alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+    src="https://wallpaper.forfun.com/fetch/4b/4b49ebebb437fa2078fb5a3aa080ee58.jpeg"
+    alt="berlim"
     width="1920"
     height="2193"
   />
   <div class="card__content | flow">
     <div class="card__content--container | flow">
-      <h2 class="card__title">Colombia</h2>
+      <h2 class="card__title">Berlim</h2>
       <p class="card__description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
-        labore laudantium deserunt fugiat numquam.
+       Uma metrópole que reinventa o passado com estilo, cheia de arte, liberdade e vida noturna inesquecível.
       </p>
     </div>
-    <button class="card__button">Read more</button>
+  
   </div>
 </article>
 <article class="card">
   <img
     class="card__background"
-    src="https://i.imgur.com/QYWAcXk.jpeg"
-    alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+    src="https://images.unsplash.com/photo-1550759825-41ba5b154aa7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2FvJTIwcGF1bG98ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000"
+    alt="sp"
     width="1920"
     height="2193"
   />
   <div class="card__content | flow">
     <div class="card__content--container | flow">
-      <h2 class="card__title">Colombia</h2>
+      <h2 class="card__title">São Paulo</h2>
       <p class="card__description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
-        labore laudantium deserunt fugiat numquam.
+      Energia que nunca para, sabores de todo o mundo e cultura em cada esquina da maior cidade do Brasil.
       </p>
     </div>
-    <button class="card__button">Read more</button>
+   
   </div>
 </article>
 <article class="card">
   <img
     class="card__background"
-    src="https://i.imgur.com/QYWAcXk.jpeg"
-    alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+    src="https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dCVDMyVCM3F1aW98ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000"
+    alt="Toquio"
     width="1920"
     height="2193"
   />
   <div class="card__content | flow">
     <div class="card__content--container | flow">
-      <h2 class="card__title">Colombia</h2>
+      <h2 class="card__title">Tóquio</h2>
       <p class="card__description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
-        labore laudantium deserunt fugiat numquam.
+      Um espetáculo de luzes, tradição e inovação, onde o futuro e o encanto japonês caminham lado a lado.
       </p>
     </div>
-    <button class="card__button">Read more</button>
+    
   </div>
 </article>
 </section>
+<!-- Footer -->
+<footer class="footer">
+  <h2 class="footer-logo">Clement's World</h2>
+
+  <div class="footer-social">
+    <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://github.com/Gigiovh" target="_blank"><i class="fab fa-github"></i></a>
+    <a href="https://youtube.com" target="_blank"><i class="fab fa-youtube"></i></a>
+  </div>
+
+  <p class="footer-copy">
+    Clement's World ©2025 Todos os direitos reservados a <a href="https://github.com/Gigiovh">Gigiovh</a>
+    <span class="heart">★</span> 
+  </p>
+</footer>
 </body>
+
 </html>
